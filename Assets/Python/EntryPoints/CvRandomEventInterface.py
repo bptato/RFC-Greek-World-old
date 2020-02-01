fddd66803b5db5a026ac8809b30c4828f27e78e5
bptato <nincsnevem662@gmail.com>
2020-02-01 16:21:45 +0100

    Fixed brothers in need event

--- Assets/Python/EntryPoints/CvRandomEventInterface.py
+++ Assets/Python/EntryPoints/CvRandomEventInterface.py
@@ -556,8 +556,6 @@ def canTriggerBrothersInNeed(argsList):
 	listResources.append(CvUtil.findInfoTypeNum(gc.getBonusInfo,gc.getNumBonusInfos(),'BONUS_IRON'))
 	listResources.append(CvUtil.findInfoTypeNum(gc.getBonusInfo,gc.getNumBonusInfos(),'BONUS_HORSE'))
 	listResources.append(CvUtil.findInfoTypeNum(gc.getBonusInfo,gc.getNumBonusInfos(),'BONUS_IVORY'))
-	listResources.append(CvUtil.findInfoTypeNum(gc.getBonusInfo,gc.getNumBonusInfos(),'BONUS_OIL'))
-	listResources.append(CvUtil.findInfoTypeNum(gc.getBonusInfo,gc.getNumBonusInfos(),'BONUS_URANIUM'))
 
 	bFound = false
 	for iResource in listResources: 
