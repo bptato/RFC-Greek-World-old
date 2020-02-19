17fb51cd5ff7f453f09ba1599067df93b2fa3c3c
bptato <nincsnevem662@gmail.com>
2020-02-19 15:00:41 +0100

    Fixed 2nd Mycenaean UHV condition

--- Assets/Python/Victory.py
+++ Assets/Python/Victory.py
@@ -762,11 +762,12 @@ class Victory:
 
 		iGameTurn = gc.getGame().getGameTurn()
 		
-		if iPlayer == iMycenae and self.getGoal(iMycenae, 0) == -1:
+		if iPlayer == iMycenae:
 			if iBuilding == con.bn('mycenae_tholoi'):
-				self.setMycenaeTombsBuilt(self.getMycenaeTombsBuilt() + 1)
-				if self.getMycenaeTombsBuilt() >= 3:
-					self.setGoal(iMycenae, 0, 1)
+				if self.getGoal(iMycenae, 0) == -1:
+					self.setMycenaeTombsBuilt(self.getMycenaeTombsBuilt() + 1)
+					if self.getMycenaeTombsBuilt() >= 3:
+						self.setGoal(iMycenae, 0, 1)
 			elif iBuilding == con.bn('lion_gate'):
 				if self.getGoal(iMycenae, 1) == -1:
 					self.setGoal(iMycenae, 1, 1)
